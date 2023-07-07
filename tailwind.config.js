e0e9b5c2980d2dea01ff28d25f0d32b20d1a9b5a
Adam Weston <adam@awcodes.com>
2023-07-07 18:07:34 -0400

    styling fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,6 @@ const preset = require('./vendor/filament/filament/tailwind.config.preset')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     presets: [preset],
-  content: ["./resources/views/**/*.blade.php", "./src/**/*.php"],
-  darkMode: "class",
+    content: ["./resources/views/**/*.blade.php", "./src/**/*.php"],
+    darkMode: "class",
 };
