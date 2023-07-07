add99ff6fa5a8273c231fc71d0a3d7c0dd7eb8e6
Adam Weston <adam@awcodes.com>
2023-07-07 18:05:51 -0400

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-const prefix = require('./vendor/filament/filament/tailwind.config.preset')
+const preset = require('./vendor/filament/filament/tailwind.config.preset')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    presets: [preset],
   content: ["./resources/views/**/*.blade.php", "./src/**/*.php"],
   darkMode: "class",
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ],
 };
