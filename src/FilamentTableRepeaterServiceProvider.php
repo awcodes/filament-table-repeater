cfed5069efe8c844ad50d66d986e6da1d2502e11
Maxim Shuguroff <5457669+shuguroff@users.noreply.github.com>
2023-11-14 15:33:47 +0300

    Add view loader for view override

--- src/FilamentTableRepeaterServiceProvider.php
+++ src/FilamentTableRepeaterServiceProvider.php
@@ -7,11 +7,30 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class FilamentTableRepeaterServiceProvider extends PackageServiceProvider
 {
+    public function boot()
+    {
+        $this->bootLoaders();
+        $this->bootPublishing();
+    }
+    
     public function configurePackage(Package $package): void
     {
         $package->name('filament-table-repeater')
             ->hasAssets()
             ->hasTranslations()
             ->hasViews();
+    }
+
+    protected function bootLoaders()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'filament-table-repeater');
+        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'filament-table-repeater');
+    }
+
+    protected function bootPublishing()
+    {
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/filament-table-repeater'),
+        ], 'filament-table-repeater');
     }
 }
